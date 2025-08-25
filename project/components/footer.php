1d35c0a937df799e550b1d5c452c9acaccb1de8f
sushmag0wda <sushmagowda636@gmail.com>
2025-08-25 12:18:11 +0530

    Initial commit

---
+++ project/components/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+
+   <p class="credit">Designed with <i class="fa fa-heart pulse" style="color: red;"></i> by <a href="https://www.linkedin.com/in/sushma-gowda-845b90339/"> sushma gowda</a></p>
+
+</footer>
